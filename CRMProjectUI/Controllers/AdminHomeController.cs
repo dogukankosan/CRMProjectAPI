6c9bf88dee340705aec3e6c236fb6e068f6ea0ab
DOĞUKAN KOŞAN <ghasab101@gmail.com>
2025-11-23 20:26:38 +0300

    CRMProjectAPI - V1
    CRMProjectAPI - V1

---
+++ CRMProjectUI/Controllers/AdminHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRMProjectUI.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CRMProjectUI/Program.cs
@@ -0,0 +1,17 @@
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+WebApplication? app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapStaticAssets();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=AdminHome}/{action=Index}/{id?}")
+    .WithStaticAssets();
+app.Run();
